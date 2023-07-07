4db0892b06f5c33e8e1a77c6f94b575a4fad98be
extrevaro <agb1894@gmail.com>
2023-07-07 16:40:02 +0200

    minor fixes

--- code/utils/launch_gcFront.sh
+++ code/utils/launch_gcFront.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH=$PATH:/home/alvaro/Matlab_2022/bin/
 
-nohup matlab -nodisplay -nodesktop -r "run('/home/alvaro/github/ConsortiumEngineeringTool/jupyter_notebook/run_gcFront.m')" > /home/alvaro/github/ConsortiumEngineeringTool/jupyter_notebook/gcfront.txt &
+nohup matlab -nodisplay -nodesktop -r "run('/home/alvaro/github/PET2PHA/code/utils/run_gcFront.m')" > /home/alvaro/github/PET2PHA/data/gcfront.txt &
